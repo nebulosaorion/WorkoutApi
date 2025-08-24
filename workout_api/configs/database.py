44da7c8e70ce8dea5ff355cd57fa530849462fb4
Miriã Evangelista <evangelista@furg.br>
2025-08-23 22:44:43 -0300

    Atualizações

--- workout_api/configs/database.py
+++ workout_api/configs/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from workout_api.configs.settings import settings  # Import correto
+from workout_api.configs.settings import settings  
 
 # Use a connection string das settings
 engine = create_async_engine(str(settings.DB_URL), echo=True)
@@ -11,3 +11,5 @@ Base = declarative_base()
 async def get_db():
     async with AsyncSessionLocal() as session:
         yield session
+
+get_session = get_db
